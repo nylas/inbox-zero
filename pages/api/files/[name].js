9ba7144564655ed16a2331937836a2b8b8764298
Avi Goldman <avrahamymgoldman@gmail.com>
2020-04-07 10:24:26 -0400

    api to download files

---
+++ pages/api/files/[name].js
@@ -0,0 +1,26 @@
+import protect from "../../../utils/middleware/protect";
+/**
+ * Retrieves a list of messages that belong to the current user which
+ * match the given parameters.
+ */
+export default protect(async (req, res) => {
+  try {
+    const id = req.query.id;
+
+    if (!id) {
+      return res.status(404).end();
+    }
+
+    const file = await req.nylas.files.find(id);
+    const fileData = await file.download();
+
+    res.send(fileData.body);
+  } catch (err) {
+    if (err.message.startsWith("Invalid id")) {
+      return res.status(404).end();
+    }
+
+    console.log(err);
+    res.status(500).end();
+  }
+});
