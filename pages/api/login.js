619d51cd023ba252610193c8d5fd845c3212e108
Avi Goldman <avrahamymgoldman@gmail.com>
2020-04-07 17:41:45 -0400

    auth ux/dx improvements

---
+++ pages/api/login.js
@@ -0,0 +1,19 @@
+import Nylas from "../../utils/nylas";
+
+/**
+ * Redirect the user to the Nylas hosted auth page to complete their authentication
+ */
+export default async (req, res) => {
+  const options = {
+    loginHint: req.query.login_hint || "",
+    redirectURI: "http://localhost:3000/api/authorize",
+    scopes: ["email.read_only", "email.modify", "email.send"]
+  };
+
+  const authUrl = Nylas.urlForAuthentication(options);
+
+  // redirect the user to the hosted auth page
+  res.writeHead(302, { Location: authUrl });
+
+  return res.end();
+};
