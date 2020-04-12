6a494c36bb94a6b83b0b26505605e7fe01f23683
Avi Goldman <avrahamymgoldman@gmail.com>
2020-04-12 19:59:33 -0400

    mark message and sender as read

--- pages/api/messages/[id].js
+++ pages/api/messages/[id].js
@@ -1,58 +1,97 @@
 import protect from "../../../utils/middleware/protect";
-import replyParser from "node-email-reply-parser";
+import cleanLabels from "../../../utils/cleanLabels";
+
 /**
  * Retrieves a list of messages that belong to the current user which
  * match the given parameters.
  */
 export default protect(async (req, res) => {
+  if (req.method === "GET") {
+    return getMessage(req, res);
+  }
+  if (req.method === "PUT") {
+    return updateMessage(req, res);
+  } else {
+    res.status(405).end();
+  }
+});
+
+async function getMessage(req, res) {
   try {
     const id = req.query.id;
-
     const fullMessage = await req.nylas.messages.find(id);
-    const fromEmailAdress = fullMessage.from[0].email;
+    const fromEmailAddress = fullMessage.from[0].email;
     const thread = await req.nylas.threads.find(fullMessage.threadId, null, {
       view: "expanded"
     });
     const senderUnreadCount = await req.nylas.messages.count({
-      from: fromEmailAdress,
+      in: "inbox",
+      from: fromEmailAddress,
       unread: true
     });
 
-    // console.log(thread.labels)
-
     res.status(200).json({
+      unread: thread.unread,
       senderUnread: senderUnreadCount > 0,
-      threadUnread: thread.unread,
       messages: thread.messages.map(message => {
-        if (message.id === fullMessage.id) {
-          return simplifyMessage({
-            active: true,
-            ...fullMessage.toJSON()
-          });
-        }
+        const isActiveMessage = message.id === fullMessage.id;
 
-        return simplifyMessage({
-          active: false,
-          ...message.toJSON()
-        });
+        return {
+          active: isActiveMessage,
+          ...simplifyMessage(isActiveMessage ? fullMessage : message)
+        };
       })
     });
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Something went wrong. Please try again." });
   }
-});
+}
+
+async function updateMessage(req, res) {
+  try {
+    const id = req.query.id;
+    const fullMessage = await req.nylas.messages.find(id);
+
+    if (req.body.unread === false) {
+      fullMessage.unread = false;
+      fullMessage.save();
+    }
+
+    if (req.body.senderUnread === false) {
+      const fromEmailAddress = fullMessage.from[0].email;
+      const unreadMessages = await req.nylas.messages.list({
+        in: "inbox",
+        from: fromEmailAddress,
+        unread: true
+      });
+
+      for (const message of unreadMessages) {
+        message.unread = false;
+        message.save();
+      }
+    }
+
+    res.status(200).end();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Something went wrong. Please try again." });
+  }
+}
 
 function simplifyMessage(message) {
   return {
-    active: message.active,
     id: message.id,
     subject: message.subject,
     from: message.from,
     date: message.date,
     unread: message.unread,
-    body: replyParser(message.body, true),
+    body: message.body,
     hasAttachments: message.hasAttachments || message.files.length > 0,
-    files: message.files || []
+    files: message.files
+      ? message.files.map(({ filename, id }) => {
+          return { filename: filename || "noname", id };
+        })
+      : []
   };
 }
--- pages/messages/[id]/index.js
+++ pages/messages/[id]/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import fetch from "isomorphic-unfetch";
 import Head from "next/head";
+import Link from "next/link";
 import InboxContainer from "../../../components/InboxContainer";
 import Header from "../../../components/Header";
 import Sidebar from "../../../components/Sidebar";
@@ -14,6 +15,8 @@ import flagIcon from "../../../assets/flag.svg";
 import doubleFlagIcon from "../../../assets/double_flag.svg";
 import chevronLeftIcon from "../../../assets/chevron_left.svg";
 import chevronRightIcon from "../../../assets/chevron_right.svg";
+import checkboxUncheckedIcon from "../../../assets/checkbox_unchecked.svg";
+import checkboxCheckedIcon from "../../../assets/checkbox_checked.svg";
 import withAuth from "../../../utils/withAuth";
 import classnames from "classnames";
 import Frame, { FrameContextConsumer } from "react-frame-component";
@@ -40,50 +43,141 @@ export default function detailsPage({ account, thread }) {
   const message = thread.messages[activeIndex];
   const prevMessages = thread.messages.slice(0, activeIndex + 1);
   const nextMessages = thread.messages.slice(activeIndex + 1);
+  const showToDoList = account.organizationUnit === "label";
 
   const [iframeHeight, setIframeHeight] = useState(500);
+  const [isUnread, setIsUnread] = useState(thread.unread);
+  const [isSenderUnread, setIsSenderUnread] = useState(thread.senderUnread);
+
+  async function markAsRead() {
+    try {
+      await fetch(`http://localhost:3000/api/messages/${message.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ unread: false })
+      });
+
+      setIsUnread(false);
+    } catch (e) {
+      alert("Something went wrong");
+    }
+  }
+
+  async function markSenderAsRead() {
+    try {
+      await fetch(`http://localhost:3000/api/messages/${message.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ senderUnread: false })
+      });
+
+      setIsUnread(false);
+      setIsSenderUnread(false);
+    } catch (e) {
+      alert("Something went wrong");
+    }
+  }
+
   return (
     <InboxContainer>
+      <Head>
+        <title>{message.subject} - Inbox Zero</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header account={account} />
       <Sidebar>
+        <Link href="/">
+          <a className={styles.BackButton}>
+            <img
+              src={chevronLeftIcon}
+              alt="Back"
+              className={styles.BackButton__icon}
+            />{" "}
+            <span>Back</span>
+          </a>
+        </Link>
         <Button>Reply</Button>
         <ul className={styles.Actions}>
           <li className={styles.Action}>
-            <span className={styles.Actions__image}>
-              <img src={calendarIcon} />
-            </span>
-            <span>Schedule Meeting »</span>
+            <button className={styles.Action__button}>
+              <span className={styles.Action__icon}>
+                <img src={calendarIcon} />
+              </span>
+              <span>Schedule Meeting »</span>
+            </button>
           </li>
-          <li className={styles.Action}>
-            <span className={styles.Actions__image}>
-              <img src={checkIcon} />
-            </span>
-            <span>Add to ToDo List »</span>
+          {showToDoList ? (
+            <li className={styles.Action}>
+              <button className={styles.Action__button}>
+                <span className={styles.Action__icon}>
+                  <img src={checkIcon} />
+                </span>
+                <span>Add to ToDo List »</span>
+              </button>
+            </li>
+          ) : (
+            ""
+          )}
+          <li>
+            <ul className={styles.Labels}>
+              {account.labels.map(({ id, displayName }) => (
+                <li className={styles.Label}>
+                  <button className={styles.Label__button}>
+                    <span className={styles.Label__icon}>
+                      <img src={checkboxCheckedIcon} />
+                    </span>
+                    <span>{displayName}</span>
+                  </button>
+                </li>
+              ))}
+            </ul>
           </li>
           <li
             className={classnames(styles.Action, {
-              [styles.enabled]: !thread.threadUnread
+              [styles.disabled]: isUnread === false
             })}
           >
-            <span className={styles.Actions__image}>
-              <img src={flagIcon} />
-            </span>
-            <span>Mark as Read »</span>
+            <button
+              className={styles.Action__button}
+              onClick={markAsRead}
+              disabled={isUnread === false}
+            >
+              <span className={styles.Action__icon}>
+                <img src={flagIcon} />
+              </span>
+              <span>Mark as Read »</span>
+            </button>
           </li>
           <li
             className={classnames(styles.Action, {
-              [styles.enabled]: !thread.senderUnread
+              [styles.disabled]: isSenderUnread === false
             })}
           >
-            <span className={styles.Actions__image}>
-              <img src={doubleFlagIcon} />
-            </span>
-            <span>Mark All Emails From Sender as Read »</span>
+            <button
+              className={styles.Action__button}
+              onClick={markSenderAsRead}
+              disabled={isSenderUnread === false}
+            >
+              <span className={styles.Action__icon}>
+                <img src={doubleFlagIcon} />
+              </span>
+              <span>Mark All Emails From Sender as Read »</span>
+            </button>
           </li>
         </ul>
       </Sidebar>
       <Main>
-        <h2 className={styles.Subject}>{message.subject}</h2>
+        <h2
+          className={classnames(styles.Subject, {
+            [styles.disabled]: isUnread === false
+          })}
+        >
+          {message.subject}
+        </h2>
         <List>
           {prevMessages.map(message => (
             <List.Message
@@ -108,15 +202,25 @@ export default function detailsPage({ account, thread }) {
             <FrameContextConsumer>
               {({ document, window }) => {
                 if (iframeHeight === 500) {
-                  setIframeHeight(window.document.documentElement.scrollHeight);
+                  setTimeout(
+                    () =>
+                      setIframeHeight(
+                        window.document.documentElement.scrollHeight
+                      ),
+                    1000
+                  );
                 }
               }}
             </FrameContextConsumer>
           </Frame>
           {message.hasAttachments && (
             <div className={styles.AttachmentWrapper}>
-              {message.files.map(ile => (
-                <a href={`/api/files/${file.id}`} className={styles.Attachment}>
+              {message.files.map(file => (
+                <a
+                  href={`/api/files/${file.filename}?id=${file.id}`}
+                  className={styles.Attachment}
+                  download
+                >
                   {file.filename}
                 </a>
               ))}
