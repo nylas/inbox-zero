19135fcf9b0e9bf83890b163d8ecd6e6dfe3137d
Avi Goldman <avrahamymgoldman@gmail.com>
2020-04-12 19:57:35 -0400

    page transition styling

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,4 +1,14 @@
+import { useRef } from "react";
+import Router from "next/router";
 import styles from "./Main.module.css";
+
+Router.events.on("routeChangeComplete", url => {
+  const mainElement = document.querySelector(`.${styles.Main}`);
+
+  if (mainElement) {
+    mainElement.scrollTo(0, 0);
+  }
+});
 
 export default function Main({ children }) {
   return <main className={styles.Main}>{children}</main>;
