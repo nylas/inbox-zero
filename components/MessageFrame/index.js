5db1a5c8683b1300d97a38161742075b8dee9b58
Avi Goldman <avrahamymgoldman@gmail.com>
2020-04-14 18:53:03 -0400

    replaced iframe package with regular iframe

--- components/MessageFrame/index.js
+++ components/MessageFrame/index.js
@@ -1,60 +1,50 @@
-import { useState, useEffect } from "react";
-import Frame, { FrameContextConsumer } from "react-frame-component";
+import { useState, useRef, useEffect } from "react";
 
 export default ({ content }) => {
-  const [count, setCount] = useState(0);
   const [iframeHeight, setIframeHeight] = useState(0);
 
-  // refresh iframe when content changes
+  const ref = useRef(null);
   useEffect(() => {
-    setCount(0);
-    setIframeHeight(0);
+    const doc = ref.current.contentWindow.document;
+    doc.body.innerHTML = content;
+
+    // add default styling
+    const style = doc.createElement("style");
+    style.innerHTML = `
+      body {
+        font-family: Roboto, RobotoDraft, Helvetica, Arial, sans-serif;
+      }
+
+      .gmail_quote {
+        display: none;
+      }
+    `;
+    doc.head.appendChild(style);
+
+    // force all links to open in a new tab
+    [...doc.links].forEach(link => {
+      link.target = "_blank";
+      link.rel = "noopener noreferrer";
+    });
+
+    setIframeHeight(doc.documentElement.scrollHeight);
+
+    // refresh height after each image loads
+    [...doc.images].forEach(image => {
+      image.onload = function() {
+        setIframeHeight(doc.documentElement.scrollHeight);
+      };
+    });
   }, [content]);
 
   return (
-    <Frame
+    <iframe
+      ref={ref}
       style={{
         width: "100%",
         border: 0,
         height: `${iframeHeight}px`
       }}
-    >
-      <FrameContextConsumer>
-        {({ document, window }) => {
-          if (count === 0) {
-            document.open();
-            document.write(content);
-            document.close();
-            // add default styling
-            const style = document.createElement("style");
-            style.innerHTML = `
-              body {
-                font-family: Roboto, RobotoDraft, Helvetica, Arial, sans-serif;
-              }
-
-              .gmail_quote {
-                display: none;
-              }
-            `;
-            document.head.appendChild(style);
-
-            // force all links to open in a new tab
-            [...document.links].forEach(link => {
-              link.target = "_blank";
-              link.rel = "noopener noreferrer";
-            });
-          }
-
-          // retry getting the frame height 10 times .5 second apart to give images time to load
-          if (count < 10) {
-            setTimeout(() => {
-              setIframeHeight(window.document.documentElement.scrollHeight);
-            }, 500 * count);
-
-            setCount(count + 1);
-          }
-        }}
-      </FrameContextConsumer>
-    </Frame>
+    />
   );
 };
