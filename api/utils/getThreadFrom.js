6e06d55998f44d1d695fce19e5da2faeea0d4e20
Avi Goldman <avrahamymgoldman@gmail.com>
2020-04-17 20:11:25 -0400

    handle case where account was the only sender

--- api/utils/getThreadFrom.js
+++ api/utils/getThreadFrom.js
@@ -3,10 +3,12 @@
  * by the current account and returns the first "from" name and email
  */
 module.exports = function getThreadFrom(thread) {
+  const lastMessageReceivedTimestamp = (
+    thread.lastMessageReceivedTimestamp || thread.lastMessageTimestamp
+  ).getTime();
+
   const lastMessageReceived = thread.messages.find(message => {
-    return (
-      message.date.getTime() === thread.lastMessageReceivedTimestamp.getTime()
-    );
+    return message.date.getTime() === lastMessageReceivedTimestamp;
   });
 
   return lastMessageReceived.from[0];
