4baf90dd9ca0b6388c016c123cf710a300e419e5
Avi Goldman <avrahamymgoldman@gmail.com>
2020-04-23 15:45:35 -0400

    added env error page

--- server.js
+++ server.js
@@ -51,6 +51,26 @@ app.prepare().then(() => {
   server.use(cookieParser());
   server.use(bodyParser.json());
 
+  /** Show a warning when the env vars aren't configured */
+  if (!process.env.NYLAS_ID || !process.env.NYLAS_SECRET) {
+    server.use("*", (req, res) => {
+      res.send(`
+        <p>
+          Add your Nylas ID and Secret in the .env file to get started. <br>
+          Don't have a Nylas account? <a href="https://dashboard.nylas.com/register">Try it free »</a>
+        </p>
+        <style>
+        body { display: flex; background: #000000; color: white; border-top: 10px solid #00E5C0; margin: 0; }
+        p    { font: 24px bold -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif;
+               margin: auto 0;
+               line-height: 1.8;
+               padding: 0 8rem 4rem; }
+        a    { color: #00e5bf; }
+        </style>
+      `);
+    });
+  }
+
   /** Serve the API endpoints with the prefix "/api" */
   server.use("/api", api);
 
